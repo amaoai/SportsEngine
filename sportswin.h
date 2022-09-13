0cc2d402c5a3dacac01b5a0d05b2bf8ee99d9ab0
2B键盘 <2968640488@qq.com>
2022-09-14 00:49:19 +0800

    FT: DOC

--- sportswin.h
+++ sportswin.h
@@ -1,13 +1,33 @@
+/* ************************************************************************
+ *
+ * Copyright (C) 2022 Vincent Luo All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ************************************************************************/
+
+/* Creates on 2022/9/14. */
 #pragma once
 
 #include "tools/logs.h"
 #include <GLFW/glfw3.h>
 
 #define SportsPollEvents() glfwPollEvents()
-#define SportsTerminate() glfwTerminate();
+#define SportsTerminate() glfwTerminate()
 
 class SportsWindow;
 
+/* window resize callback */
 typedef void (*SportsfnSetWindowResizeCallback) (SportsWindow *sportswin, int w, int h);
 
 class SportsWindow {
